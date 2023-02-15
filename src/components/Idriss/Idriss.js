b3d3440fc81294f7eeb9b46703487995585b7951
55973 <55973@etu.he2b.be>
2023-02-15 10:14:53 +0100

    1er commit

--- src/components/Idriss/Idriss.js
+++ src/components/Idriss/Idriss.js
@@ -1,10 +1,27 @@
-import './_idriss.scss'
 
 function Idriss() {
 
-  return(
+  return (
     <div className="Idriss">
-      
+      <div className="w-full h-screen flex justify-center items-center ">
+        <div className="w-[60%] h-[70%] flex">
+
+          <div className="bg-blue-400 w-[30%]">
+            <ul>
+              <li>a</li>
+              <li>b</li>
+              <li>c</li>
+              <li>d</li>
+            </ul>
+          </div>
+
+          <div className="w-[70%]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae earum reiciendis obcaecati ducimus cumque vero, ipsa, sint dolor possimus optio qui? Nobis itaque quibusdam blanditiis adipisci quod necessitatibus eum corporis.
+            <Yassin/>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 
